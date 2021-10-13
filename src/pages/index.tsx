0f2aed084a20a35ba339f8e050638cfaea582347
Vinicius de Souza Santos <vinicius.santos@aztronic.com.br>
2021-10-12 22:22:45 -0300

    Página: Sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,56 @@
+import { Flex, Input, Button, Stack, FormControl, FormLabel } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bg: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bg: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Login
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
