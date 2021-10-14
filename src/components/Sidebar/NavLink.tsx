597d762d951f8c9a007c6ecda34adfcf1c61d88a
Vinicius de Souza Santos <vinicius.santos@aztronic.com.br>
2021-10-14 20:05:33 -0300

    Sinalizando link ativo

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,6 @@
 import { Link as ChakraLink, Icon, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType } from 'react';
-import Link from 'next/link';
+import { Activelink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
@@ -10,11 +10,11 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function Navlink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <Activelink href={href} passHref>
       <ChakraLink display="flex" align="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">{children}</Text>
       </ChakraLink>
-    </Link>
+    </Activelink>
   )
 }
