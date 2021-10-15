c2637a5a508271e4897c9322cd9eec485f57b55c
Vinicius de Souza Santos <vinicius.santos@aztronic.com.br>
2021-10-14 21:31:18 -0300

    Form de criação de usuário

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,11 +1,40 @@
 import Link from 'next/link';
 import Head from "next/head";
 import { Box, Button, Divider, Flex, Heading, VStack, HStack, SimpleGrid } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
+
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatória'),
+  email: yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+  password: yup.string().required('Senha obrigatória').min(6, 'Senha deve ter no mínimo 6 caracteres'),
+  password_confirmation: yup.string().oneOf([null, yup.ref('password')], 'Senhas não conferem'),
+});
+
 export default function CreateUser() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema),
+  });
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (data) => {
+    event.preventDefault();
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  };
+
   return (
     <>
       <Head>
@@ -18,7 +47,7 @@ export default function CreateUser() {
         <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
           <Sidebar />
 
-          <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+          <Box as="form" flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]} onSubmit={handleSubmit(handleCreateUser)}>
             <Heading size="lg" fontWeight="normal">
               Criar usuário
             </Heading>
@@ -27,13 +56,36 @@ export default function CreateUser() {
 
             <VStack spacing="8">
               <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-                <Input name="name" label="Nome completo" />
-                <Input type="email" name="email" label="E-mail" />
+                <Input
+                  name="name"
+                  label="Nome completo"
+                  {...register('name')}
+                  error={formState.errors.name}
+                />
+                <Input
+                  type="email"
+                  name="email"
+                  label="E-mail"
+                  {...register('email')}
+                  error={formState.errors.email}
+                />
               </SimpleGrid>
 
               <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-                <Input type="password" name="password" label="Senha" />
-                <Input type="password" name="password_confirmation" label="Confirmação da senha" />
+                <Input
+                  type="password"
+                  name="password"
+                  label="Senha"
+                  {...register('password')}
+                  error={formState.errors.password}
+                />
+                <Input
+                  type="password"
+                  name="password_confirmation"
+                  label="Confirmação da senha"
+                  {...register('password_confirmation')}
+                  error={formState.errors.password_confirmation}
+                />
               </SimpleGrid>
             </VStack>
 
@@ -42,7 +94,8 @@ export default function CreateUser() {
                 <Link href="/users" passHref>
                   <Button colorScheme="whiteAlpha">Cancelar</Button>
                 </Link>
-                <Button colorScheme="pink">Salvar</Button>
+
+                <Button type="submit" colorScheme="pink" isLoading={formState.isSubmitting}>Salvar</Button>
               </HStack>
             </Flex>
           </Box>
